0f0240a815c57dd724f8392db4b794ae3faee265
a114221004-shu <a114221004@mail.shu.edu.tw>
2026-03-19 16:00:55 +0800

    115-03-19

---
+++ 115_03_19/Tutorial Ex1-7-2-1/Test Average/Test Average/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Average 方法接受一個 int 陣列作為參數
+        // 並回傳陣列中值的平均。
+        private double Average(int[] sArray , int count  )
+        {
+            int total = 0;
+            for ( int i = 0; i < count; i++)
+            {
+                total += sArray[i];
+            }
+            return (double)total / sArray.Length;
+
+        }
+
+        // Highest 方法接受一個 int 陣列作為參數
+        // 並回傳該陣列中的最大值。
+        private  int Highest(int[] sArray , int count  )
+        {
+            int highScore = sArray[0];
+            for (int i = 1; i < count; i++)
+            {
+                if (sArray[i] > highScore)
+                {
+                    highScore = sArray[i];
+                }
+            }
+            return highScore;
+
+        }
+
+        // Lowest 方法接受一個 int 陣列作為參數
+        // 並回傳該陣列中的最小值。
+        private int Lowest(int[] sArray , int count)
+        {
+           int lowScore = sArray[0];
+            for (int i = 1; i < count; i++)
+            {
+                if (sArray[i] < lowScore)
+                {
+                    lowScore = sArray[i];
+                }
+            }
+            return lowScore;
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 60;
+            int[] scores = new int[SIZE];
+            StreamReader inputFile;
+            int index = 0;
+
+            try
+            {
+                inputFile = File.OpenText("TestScores.txt");
+
+                while (!inputFile.EndOfStream && index < scores.Length)
+                {
+                    scores[index] = int.Parse(inputFile.ReadLine());
+                    index++;
+                }
+                inputFile.Close();
+               for (int i = 0; i< index; i++)
+                {
+                    testScoresListBox.Items.Add(scores [i] );
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //double averageScore = Average(scores);
+            averageScoreLabel.Text = Average(scores,index).ToString("n1");
+            highScoreLabel.Text = Highest(scores,index).ToString();
+            lowScoreLabel.Text = Lowest(scores,index).ToString();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+
+        private void testScoresListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
