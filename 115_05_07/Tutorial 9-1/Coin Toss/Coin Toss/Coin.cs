3755b1a1c008d878a1614d0d80a499f31a929464
a114221004-shu <a114221004@mail.shu.edu.tw>
2026-05-07 16:00:39 +0800

    115-05-07

---
+++ 115_05_07/Tutorial 9-1/Coin Toss/Coin Toss/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin_Toss
+{
+    internal class Coin
+    {
+        private string sideUp; // 正面或反面
+        
+        public Coin()
+        {
+            // 將硬幣初始化為隨機面向。
+            sideUp = "Heads"; // 預設為正面
+        }
+
+        public void Toss()
+        {
+            Random rand = new Random();
+            if (rand.Next(2) == 0)
+            {
+                sideUp = "Heads";
+            }
+            else
+            {
+                sideUp = "Tails";
+            }
+        }
+
+        public string GetSideUp()
+        {
+            return sideUp;
+        }
+    }
+}
---
+++ 115_05_07/Tutorial 9-1/Coin Toss/Coin Toss/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Coin_Toss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.tossButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.ItemHeight = 45;
+            this.outputListBox.Location = new System.Drawing.Point(30, 17);
+            this.outputListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(358, 184);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // tossButton
+            // 
+            this.tossButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tossButton.Location = new System.Drawing.Point(30, 242);
+            this.tossButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tossButton.Name = "tossButton";
+            this.tossButton.Size = new System.Drawing.Size(165, 66);
+            this.tossButton.TabIndex = 1;
+            this.tossButton.Text = "擲五次";
+            this.tossButton.UseVisualStyleBackColor = true;
+            this.tossButton.Click += new System.EventHandler(this.tossButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.exitButton.Location = new System.Drawing.Point(225, 242);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(165, 66);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "結束";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 332);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.tossButton);
+            this.Controls.Add(this.outputListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "擲硬幣";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button tossButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
